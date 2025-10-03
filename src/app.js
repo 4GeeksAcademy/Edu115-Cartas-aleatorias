0d38a556bcea557553424229ba4bfa00cec89468
Edupe2603 <31553111+Edupe2603@users.noreply.github.com>
2025-10-03 23:56:44 +0000

    Generador de cartas de Edu primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numero = document.getElementById("numero")
+  let centro = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+  let escogeCentro = Math.floor(Math.random() * centro.length)
+  let centroEscogido = centro[escogeCentro]
+  numero.textContent = centroEscogido
+
+  let palosConColor = [
+    { palo: "♠️", color: "black" },
+    { palo: "♣️", color: "black" },
+    { palo: "♥️", color: "red" },
+    { palo: "♦️", color: "red" },
+  ]
+
+  let escogePaloConColor = Math.floor(Math.random() * palosConColor.length)
+  let paloConColorEscogido = palosConColor[escogePaloConColor]
+  let palo1 = document.getElementById("palo1")
+  let palo2 = document.getElementById("palo2")
+
+  palo1.innerText = paloConColorEscogido.palo
+  palo1.style.color = paloConColorEscogido.color
+  palo2.innerText = paloConColorEscogido.palo
+  palo2.style.color = paloConColorEscogido.color
+  numero.style.color = paloConColorEscogido.color
 };
